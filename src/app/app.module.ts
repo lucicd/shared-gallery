b2ad5132656b5051ec845b0faaff3614b428a64a
lucicd <lucicd@hotmail.com>
2021-07-06 11:56:21 +0400

    Added login form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
-import { AppComponent } from './app.component';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { ButtonModule } from 'primeng/button';
 import { MenubarModule } from 'primeng/menubar';
 import { MenuBarComponent } from './shared/menu-bar/menu-bar.component';
 import { SharedModule } from 'primeng/api';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
 
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { PasswordModule } from 'primeng/password';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuBarComponent
+    MenuBarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
+    AppRoutingModule,
     ButtonModule,
     MenubarModule,
-    SharedModule
+    SharedModule,
+    CardModule,
+    InputTextModule,
+    ProgressSpinnerModule,
+    PasswordModule
   ],
   providers: [],
   bootstrap: [AppComponent]
